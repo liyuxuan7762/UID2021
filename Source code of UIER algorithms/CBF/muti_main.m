343259c302589e2f86f1d9a7123af8ca95f5db91
Li Yuxuan <77979684+liyuxuan7762@users.noreply.github.com>
2022-11-20 19:45:54 +0800

    Add files via upload

---
+++ Source code of UIER algorithms/CBF/muti_main.m
@@ -0,0 +1,26 @@
+clear all;
+close all;
+clc;
+fatherPath=['C:\Users\¿ÓˆŒΩ‹\Desktop\work\Xinjie Li-20210222\Source Images_0227'];
+dirs=dir(fatherPath);
+dircell=struct2cell(dirs);
+for ii=3:length(dircell)
+        subdirs=dircell(1,ii);
+        SonPath=[fatherPath '\' cell2mat(subdirs)];
+        %SonPath=[fatherPath?'\'?cell2mat(subdirs)?'\*.pgm'];
+        dirs2=dir(SonPath);
+        dircell2=struct2cell(dirs2);
+        for jj=3:length(dirs2)
+               PictureName=dircell2(1,jj);
+               PicturePath=[SonPath '\' cell2mat(PictureName)];
+               I=imread(PicturePath);
+                %?imgname=[SonPath?'\'?cell2mat(PictureName)?'.png'];
+                
+                
+               J=main_2(I);
+                
+              sav_name=cell2mat(PictureName);
+             imgname=['C:\Users\¿ÓˆŒΩ‹\Desktop\work\Xinjie Li-20210222\Enhanced or Restored Images_0227\' cell2mat(subdirs) '\' 'ERI' sav_name(3:length(sav_name)-4) '_CBF.png'];
+                imwrite(J,imgname);
+        end
+end
